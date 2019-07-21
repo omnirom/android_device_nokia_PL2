4c24bcb5055465292362624a430d7a13f5b3f6d0
Lonnie <MostlyGhostly1000@gerrit.omnirom.org>
2019-07-21 22:07:13 +0100

    Revert "PL2: Drop unused bootanimation flags"
    This reverts commit 5ac44785a7fd51f9a9d38a8cb3f1e19c6b24191c.
    
    Reason for revert: <INSERT REASONING HERE>
    
    Change-Id: I9d08e717944f702f8597bb82d42fb1c9ef346279

--- omni_PL2.mk
+++ omni_PL2.mk
@@ -34,6 +34,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o.mk)
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1080
+
 # Include Bootanimation configuration
 TARGET_BOOTANIMATION_SIZE :=1080p
 
